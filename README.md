b7256c248a61e3c63b42a091eff319666f38bd2d
akirasupr <vishal.rockstar7011@gmail.com>
2021-11-21 01:17:16 +0530

    unlocker: p4 release

--- README.md
+++ README.md
@@ -6,9 +6,6 @@
 
 #### Note: Unlock Game Graphics/FPS. This module will not work if You're using MagiskHide Props. Or others similar Modules with it. Not Work for All Games, causing SafetyNet fail. Need Xposed module to fix or bypass SafetyNet. It may break some system Apps, such as Miui Camera, Package Installer and etc.
 
-
-
-
 ##### ✓ INSTALLATION: Just flash via Magisk and reboot
 
 ##### Disclaimer: Naturally, you take all the responsibility for what happens to your device when you start messing around with things. I (Akira) will not be responsible for ANY damage caused to anyone's devices due to the use of this module.
--- post-fs-data.sh
+++ post-fs-data.sh
@@ -2,26 +2,26 @@
 MODULE=/data/adb/modules
 # Conflict module remover
 for REMOVE in $MODULE/*Game_Unlock*; do
-    touch $REMOVE/remove;
+    touch $REMOVE/disable;
 done;
 for REMOVE in $MODULE/*Pubg_extrem*; do
-    touch $REMOVE/remove;
+    touch $REMOVE/disable;
 done;
 for REMOVE in $MODULE/*pixel*; do
-    touch $REMOVE/remove;
+    touch $REMOVE/disable;
 done;
 for REMOVE in $MODULE/*Pixel*; do
-    touch $REMOVE/remove;
+    touch $REMOVE/disable;
 done;
 for REMOVE in $MODULE/*PIXEL*; do
-    touch $REMOVE/remove;
+    touch $REMOVE/disable;
 done;
 for REMOVE in $MODULE/*spoof*; do
-    touch $REMOVE/remove;
+    touch $REMOVE/disable;
 done;
 for REMOVE in $MODULE/*SPOOF*; do
-    touch $REMOVE/remove;
+    touch $REMOVE/disable;
 done;
 for REMOVE in $MODULE/*Spoof*; do
-    touch $REMOVE/remove;
+    touch $REMOVE/disable;
 done
