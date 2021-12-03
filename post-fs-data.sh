0e4032d800a53b4431ac351f9d6d6d852cb6c060
akirasupr <vishal.rockstar7011@gmail.com>
2021-12-04 03:07:43 +0530

    post-fs-data: fix

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 module=/data/adb/modules/
-conflict=$(find /data/adb -iname "*.prop")
+conflict=$(find /data/adb -not -path -iname "*.prop")
 for i in $conflict; do
    search=$(echo "$i" | sed -e 's/\// /g' | awk '{print $4}')
    if grep -q 'model' $i 2>/dev/null; then
