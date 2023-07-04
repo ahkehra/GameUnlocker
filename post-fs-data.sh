79fc1934ac7ab87527b6edb8507f061fed34c2c0
reallyakera <vishal.rockstar7011@gmail.com>
2023-07-04 14:39:42 +0530

    mod: revert

--- post-fs-data.sh
+++
@@ -1,9 +0,0 @@
-#!/system/bin/sh
-module=/data/adb/modules/
-conflict=$(find /data/adb -not -path -iname "*.prop")
-for i in $conflict; do
-   search=$(echo "$i" | sed -e 's/\// /g' | awk '{print $4}')
-   if grep -q 'model' $i 2>/dev/null; then
-     touch $module$search/remove
-   fi
-done
